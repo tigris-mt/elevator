cbb016550d9c331d2feaa46a07944672c112cc3d
Gabriel Pérez-Cerezo <gabriel@gpcf.eu>
2017-06-07 16:03:04 +0200

    Added recipe that uses hemp rope instead of chains

--- init.lua
+++ init.lua
@@ -877,6 +877,33 @@ if technic_path and chains_path then
             {"chains:chain", "default:diamond", "chains:chain"}
         },
     })
-else
-    -- Recipes without technic & chains required.
+elseif technic_path and farming and farming.mod and farming.mod == "redo" then
+   -- add alternative recipe with hemp rope
+       minetest.register_craft({
+        output = "elevator:elevator",
+        recipe = {
+            {"technic:cast_iron_ingot", "farming:hemp_rope", "technic:cast_iron_ingot"},
+            {"technic:cast_iron_ingot", "default:mese_crystal", "technic:cast_iron_ingot"},
+            {"technic:stainless_steel_ingot", "default:glass", "technic:stainless_steel_ingot"},
+        },
+    })
+
+    minetest.register_craft({
+        output = "elevator:shaft",
+        recipe = {
+            {"technic:cast_iron_ingot", "default:glass"},
+            {"default:glass", "farming:hemp_rope"},
+        },
+    })
+
+    minetest.register_craft({
+        output = "elevator:motor",
+        recipe = {
+            {"default:diamond", "technic:control_logic_unit", "default:diamond"},
+            {"default:steelblock", "technic:motor", "default:steelblock"},
+            {"farming:hemp_rope", "default:diamond", "farming:hemp_rope"}
+        },
+    })
+
+   -- Recipes without technic & chains required.
 end
